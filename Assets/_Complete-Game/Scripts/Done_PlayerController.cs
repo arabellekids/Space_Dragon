2395fad8bbf8151817c114d14a7f9f19e891f133
Aaron Smith <arabellekids@gmail.com>
2019-08-22 19:06:13 -0400

    updated to 2019.2

--- Assets/_Complete-Game/Scripts/Done_PlayerController.cs
+++ Assets/_Complete-Game/Scripts/Done_PlayerController.cs
@@ -1,53 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[System.Serializable]
-public class Done_Boundary 
-{
-	public float xMin, xMax, zMin, zMax;
-}
-
-public class Done_PlayerController : MonoBehaviour
-{
-	public float speed;
-	public float tilt;
-	public Done_Boundary boundary;
-
-	public GameObject shot;
-	public Transform shotSpawn;
-	public float fireRate;
-	 
-	private float nextFire;
-    Rigidbody rb;
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-    void Update ()
-	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			GetComponent<AudioSource>().Play ();
-		}
-	}
-
-	void FixedUpdate ()
-	{
-		float acceleration = Input.GetAxis ("Acceleration");
-		float moveVertical = Input.GetAxis ("Vertical");
-        float moveHorizontal = Input.GetAxis("Horizontal");
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		GetComponent<Rigidbody>().velocity = movement * speed;
-		
-		//GetComponent<Rigidbody>().position = new Vector3
-		//(
-		//	Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
-		//	0.0f, 
-		//	Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-		//);
-		
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
-	}
-}
+﻿using UnityEngine; using System.Collections;  [System.Serializable] public class Done_Boundary  { 	public float xMin, xMax, zMin, zMax; }  public class Done_PlayerController : MonoBehaviour { 	public float speed; 	public float tilt; 	//public Done_Boundary boundary;  	public GameObject shot; 	public Transform shotSpawn; 	public float fireRate; 	  	private float nextFire;     Rigidbody rb;     private void Start()     {         rb = GetComponent<Rigidbody>();     }     void Update () 	{ 		if (Input.GetButton("Fire1") && Time.time > nextFire)  		{ 			nextFire = Time.time + fireRate; 			Instantiate(shot, shotSpawn.position, shotSpawn.rotation); 			GetComponent<AudioSource>().Play (); 		} 	}  	void FixedUpdate () 	{ 		float acceleration = Input.GetAxis ("Acceleration"); 		float moveVertical = Input.GetAxis ("Vertical");         float moveHorizontal = Input.GetAxis("Horizontal"); 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 		GetComponent<Rigidbody>().velocity = movement * speed; 		 		//GetComponent<Rigidbody>().position = new Vector3 		//( 		//	Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),  		//	0.0f,  		//	Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax) 		//); 		 		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt); 	} } 
